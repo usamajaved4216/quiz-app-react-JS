473878de1f3b69a937f2aca53ca2fc0a4da3909e
usamajaved4216 <usamajaved4216@gmail.com>
2023-12-31 11:03:24 +0500

    do practic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+const[questions, setQuestions] = useState ([])
+const[currentIndex, setCurrentIndex] = useState (0)
+const[ inputValue, setInputValue] = useState('')
+const [score, setScore] = useState (0)
+
+
+useEffect(function(){
+  getDataFromAPI ()
+},[])
+
+function getDataFromAPI() {
+  fetch('https://the-trivia-api.com/v2/questions')
+  .then(res => res.json())
+  .then(res =>{
+   setQuestions(res)
+   res.map(function(item){
+    item.options= [...item.incorrectAnswers, item.correctAnswer]
+    item.options =  shuffle(item.options)
+   })
+    console.log(res);
+  })
+  
+}
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  
+  while (currentIndex > 0) {
+
+    
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function nextBtn() {
+  if (inputValue === ''){
+    return alert ('please select any option')
+  }
+  setCurrentIndex (currentIndex + 1) 
+if(questions[currentIndex].correctAnswer === inputValue){
+  setScore(score+ 10)
+}
+setInputValue("")
+
+
+ 
+
+
+}
+function restartBtn() {
+  setCurrentIndex (0) 
+
+}
+if(!questions.length){
+  return<h1>LODING API...</h1>
+  
+}
+ function target(e) {
+  const inp = e.target.value
+  setInputValue(inp)
+ }
+const QuizEnded= currentIndex === questions.length
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    { !QuizEnded ? 
+      <div className='App-header'>
+        <h1>Quiz app</h1>
+        <h2>Q{currentIndex + 1} : {questions [currentIndex].question.text} </h2> 
+        
+        {questions[currentIndex].options.map(function(item){
+          return <div> 
+            <input  onChange={target} name='q' type='radio' value={item} checked={inputValue === item} /> {item}
+          </div>
+        })}
+        
+        <button onClick={nextBtn} >next</button>
+        
+
+      </div> : <div>
+        <h2>Percentage</h2>
+        <h3>your score : {score}</h3>
+        <button onClick={restartBtn} > restart </button>
+        </div>
+        }
     </div>
-  );
+  )
 }
 
 export default App;
